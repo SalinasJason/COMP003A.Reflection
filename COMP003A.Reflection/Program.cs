f153ec743dddf9a3ab58c28c59769cf8dfb8cc4d
SalinasJason <jasonsalinas770@gmail.com>
2025-02-24 18:56:39 -0800

    Added questions and answers

--- COMP003A.Reflection/Program.cs
+++ COMP003A.Reflection/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace COMP003A.Reflection
+﻿// Author: Jason Salinas
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: 7.2 Coding Assignment
+namespace COMP003A.Reflection
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("7.2 Coding Assignment\n");
+
+            Console.WriteLine("1. What worked well for you over the last 8 weeks?");
+            Console.WriteLine("A: For me the examples during class help me better understand about the topic that week.\n ");
+           
+            Console.WriteLine("2. What external resources did you use to aid your learning?");
+            Console.WriteLine("A: I prefer how the Corner C# website looks and provides their information so I usually go on google and type whatever I need more inforation about like for example Looping statements I'll type 'Looping statements C# Corner'.\n");
+           
+            Console.WriteLine("3. What would you change about the course?");
+            Console.WriteLine("A: I think everything is good.\n");
+            
+            Console.WriteLine("4. Did you learn what you expected to learn in this course?");
+            Console.WriteLine("A: Yes, I expected to learn about C# and I did.\n");
+            
+            Console.WriteLine("5. What concepts were new to you?");
+            Console.WriteLine("A: Everything was new to me in the course except loops and if else statements.\n");
+            
+            Console.WriteLine("6. What concepts have you encountered already in the past?");
+            Console.WriteLine("A: I encountered loops and if else statements before in the javascript class last semester. \n");
+           
+            Console.WriteLine("7. Do you have any tips/comments for the instructor?");
+            Console.WriteLine("A: I think everything is great especially the feedbacks and examples. \n");
         }
     }
 }
